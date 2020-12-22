f41ec0724853ee6fbc0e994836a5a86498d9a1a6
Ishdip <75232076+Ishdip@users.noreply.github.com>
2020-12-21 19:20:17 -0800

    Add files via upload

---
+++ chain.js
@@ -0,0 +1,20 @@
+const Cosntraint = Matter.Constraint
+class Chain {
+    constructor(bodyA, bodyB) {
+    var options = {
+    bodyA: bodyA,
+    bodyB: bodyB,
+    stiffness:0.04,
+    length: 10
+    }
+    this.chain = Constraint.create(options);
+    World.add(world,this.chain);
+    }
+
+    display(){
+    var pointA = this.chain.bodyA.position;
+var pointB = this.chain.bodyB.position;
+strokeWeight(4);
+line(pointA.x, pointA.y, pointB.x, pointB.y)
+}
+}
